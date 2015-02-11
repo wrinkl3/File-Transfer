a302ca9b9d06a390dd567e91f3e47591b5c1f139
wrinkl3 <al.seward@gmail.com>
2015-02-11 07:36:52 +0400

    rewrite stuff

--- fileclient.c
+++ fileclient.c
@@ -1,38 +1,53 @@
+/*
+ * name: Alex Shatberashvili	
+ */
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <libgen.h> 
 
 //fileclient ip_of_fileserver port_to_connect file_to_send
 int main(int argc, char *argv[]){
-	int sockfd, portnum, n, s;
-    struct sockaddr_in serv_addr;
-    char buffer[256];
+	int sockfd, portnum, s;
+	char *server_ip, *file_addr, *file_name;
+	struct sockaddr_in serv_addr;
 
-    if (argc < 4) {
-       fprintf(stderr, "insufficient ammount of arguments\n");
-       exit(0);
+	//checking the arg count
+	if (argc < 4) {	
+		fprintf(stderr,"insufficient arguments\n");
+		exit(0);
+	}
+	//fetch the arguments
+	server_ip = argv[1];
+	file_addr = argv[3];
+	file_name = basename(file_addr);
+	portnum = atoi(argv[2]);
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0){ 
+        fprintf(stderr,"ERROR opening socket");
+        exit(0);
     }
-    //create the socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        perror("ERROR opening socket");
-
-    //fill in the address info
-    memset((char *) &serv_addr, '\0', sizeof(serv_addr));
-    portnum = atoi(argv[2]);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(portnum);
-  	s = inet_pton(AF_INET, argv[1], &(serv_addr.sin_addr));
+	//fill in the socket
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(portnum);
+	s = inet_pton(AF_INET, server_ip, &(serv_addr.sin_addr));
   	if (s <= 0) {
         if (s == 0)
             fprintf(stderr, "Not in presentation format");
         else
-            perror("inet_pton");
+            fprintf(stderr,"inet_pton");
         exit(EXIT_FAILURE);
     }
-    //printf("%u\n", serv_addr.sin_addr.s_addr);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
+        fprintf(stderr, "ERROR connecting");
+        exit(0);
+    }
+    return 0;
 }
--- fileserver.c
+++ fileserver.c
@@ -1,3 +1,6 @@
+/*
+ * name: Alex Shatberashvili	
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,36 +9,48 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+ static int BUFFER_SIZE = 256;
+
 //fileserver ip_to_bind port_to_listen
 int main(int argc, char *argv[]){
-	int sockfd, portnum, n, s;
-    struct sockaddr_in serv_addr;
-    char buffer[256];
-
-    if (argc < 2) {
-         fprintf(stderr, "insufficient ammount of arguments\n");
-         exit(1);
-     }
-     //create the socket
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        perror("ERROR opening socket");
-
-    //fill in the address info
-    memset((char *) &serv_addr, '\0', sizeof(serv_addr));
-    portnum = atoi(argv[2]);
+	int sockfd, newsockfd, portnum, n, s, clilen;
+	char *ip_addr;
+	struct sockaddr_in serv_addr, cli_addr;
+	char buffer[BUFFER_SIZE];
+	//check the arg count
+	if (argc < 2) {
+		fprintf(stderr, "insufficient ammount of arguments\n");
+		exit(0);
+	}
+	//fetch the arguments
+	portnum = atoi(argv[2]);
+	ip_addr = argv[1];
+	//create the socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0){ 
+        fprintf(stderr,"ERROR opening socket");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(portnum);
-  	s = inet_pton(AF_INET, argv[1], &(serv_addr.sin_addr));
+    s = inet_pton(AF_INET, ip_addr, &(serv_addr.sin_addr));
   	if (s <= 0) {
         if (s == 0)
             fprintf(stderr, "Not in presentation format");
         else
-            perror("inet_pton");
+            fprintf(stderr,"inet_pton");
         exit(EXIT_FAILURE);
     }
-    //bind
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, 
-		sizeof(serv_addr)) < 0) 
-			perror("ERROR on binding");
+	if(bind(sockfd, (struct sockaddr *) &serv_addr, 
+		sizeof(serv_addr)) < 0){ 
+		fprintf(stderr, "ERROR on binding");
+			exit(0);
+    	}
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+	newsockfd = accept(sockfd, 
+	(struct sockaddr *) &cli_addr, &clilen);
+
+     exit(0);
 }
