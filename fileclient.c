6e3db306b3979531eeb2d0ad1a6595d6fdb658dd
wrinkl3 <al.seward@gmail.com>
2015-02-11 08:54:34 +0400

    drawn up

--- fileclient.c
+++ fileclient.c
@@ -13,9 +13,10 @@
 
 //fileclient ip_of_fileserver port_to_connect file_to_send
 int main(int argc, char *argv[]){
-	int sockfd, portnum, s;
+	int sockfd, portnum, s, flen, size;
 	char *server_ip, *file_addr, *file_name;
 	struct sockaddr_in serv_addr;
+	FILE *fp;
 
 	//checking the arg count
 	if (argc < 4) {	
@@ -25,7 +26,7 @@ int main(int argc, char *argv[]){
 	//fetch the arguments
 	server_ip = argv[1];
 	file_addr = argv[3];
-	file_name = basename(file_addr);
+	filename = basename(file_addr);
 	portnum = atoi(argv[2]);
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -45,9 +46,34 @@ int main(int argc, char *argv[]){
             fprintf(stderr,"inet_pton");
         exit(EXIT_FAILURE);
     }
+    //connect to the server
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
         fprintf(stderr, "ERROR connecting");
         exit(0);
     }
+    else{
+    	printf("connected to the server");
+    }
+    //open the file
+    fp = fopen(file_addr, "rb");
+    if(fp == NULL){
+    	fprintf(stderr, "ERROR opening the file");
+        exit(0);
+    }
+    //get file size
+	fseek(fp, 0L, SEEK_END);
+	size = ftell(fp);
+	fseek(fp, 0L, SEEK_SET);
+    //count the name string length
+    flen = strlen(filename);
+    flen++; //account for the null terminator
+    //send header info
+    write(sockfd, &flen, sizeof(int));
+    write(sockfd, filename, flen);
+    //send the file
+    sendfile(sockfd, fp, size);
+    fclose(fp);
+    close(sockfd);
+
     return 0;
 }
--- fileserver.c
+++ fileserver.c
@@ -10,13 +10,15 @@
 #include <netinet/in.h>
 
  static int BUFFER_SIZE = 256;
+ static int DEFAULT_CHUNCK_SIZE = 64;
 
 //fileserver ip_to_bind port_to_listen
 int main(int argc, char *argv[]){
-	int sockfd, newsockfd, portnum, n, s, clilen;
-	char *ip_addr;
+	int sockfd, newsockfd, portnum, s, clilen, flen, data_recieved;
+	char *ip_addr, *filename;
 	struct sockaddr_in serv_addr, cli_addr;
 	char buffer[BUFFER_SIZE];
+	FILE *fp;
 	//check the arg count
 	if (argc < 2) {
 		fprintf(stderr, "insufficient ammount of arguments\n");
@@ -47,10 +49,36 @@ int main(int argc, char *argv[]){
 		fprintf(stderr, "ERROR on binding");
 			exit(0);
     	}
+    printf("Listening for connection")
     listen(sockfd,5);
     clilen = sizeof(cli_addr);
 	newsockfd = accept(sockfd, 
 	(struct sockaddr *) &cli_addr, &clilen);
+	if(newsockfd<=0){
+		fprintf(stderr, "Not in presentation format");
+		exit(0);
+	} 
+	else{
+		printf("Accepted Connection");
+	}
+	//get the filename length
+	read(newsockfd, &flen, sizeof(int));
+	//get the filename
+	char filename[flen];
+	read(newsockfd, filename, flen);
+	//create the new file to be copied into
+	fp = fopen(filename, "wb");
+	if(fp == NULL){
+		fprintf(stderr, "Can't create the file");
+		exit(0);
+	}
+	while((data_received = read(newsockfd, buffer, BUFFER_SIZE)) >0){
+		fwrite(buffer, 1, data_received, fp);
+	}
 
-     exit(0);
+
+	fclose(fp);
+	close(newsockfd);
+	close(sockfd);
+	exit(0);
 }
